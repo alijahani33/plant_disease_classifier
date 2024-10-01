b6c881dc7c4550a0779469c6eb37ee49199a41d5
alijahani33 <alijahani1919@gmail.com>
2024-10-01 04:59:34 +0330

    add some codes

---
+++ config.py
@@ -0,0 +1,12 @@
+#Dataset configuration
+DATASET_PATH = './data/raw/plantvillage'
+IMAGE_SIZE = (256, 256)
+BATCH_SIZE = 16
+
+#Model_configuration
+EPOCHS = 55
+CHANNELS = 3
+N_CLASSES = 3
+
+#Path
+MODEL_SAVE_PATH = './models/potatoes.h5'
